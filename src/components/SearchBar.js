0d0443cdf6706740aedde69aa2f347a8b326a542
Hugodesousa <hugodesousa.16@gmail.com>
2022-08-22 18:38:49 -0300

    requisito 10

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,6 +4,37 @@ export default function SearchBar() {
   return (
     <div>
       <input type="text" data-testid="search-input" />
+      <label htmlFor="search-radio">
+        ingredient
+        <input
+          name="search-radio"
+          type="radio"
+          id="1"
+          data-testid="ingredient-search-radio"
+          value=""
+        />
+        Name
+        <input
+          name="search-radio"
+          type="radio"
+          id="2"
+          data-testid="name-search-radio"
+        />
+        First letter
+        <input
+          name="search-radio"
+          type="radio"
+          id="3"
+          data-testid="first-letter-search-radio"
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="exec-search-btn"
+      >
+        Buscar
+      </button>
+
     </div>
   );
 }
