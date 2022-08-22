f329a48826d6ffc901deceef251de1dbcb6dba90
Guilherme Palma <guilhermepallma@gmail.com>
2022-08-22 15:11:09 -0400

    trocando de branch

--- src/Pages/Drinks.js
+++ src/Pages/Drinks.js
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
-export default function Foods({ location }) {
+export default function Drinks({ location }) {
   return (
     <div>
-      { location.pathname === '/foods' ? <Header title="Foods" iconSearch /> : <> </> }
+      { location.pathname === '/drinks' ? <Header title="Drinks" iconSearch /> : <> </> }
     </div>
   );
 }
-Foods.propTypes = {
+Drinks.propTypes = {
   location: PropTypes.shape({
     pathname: PropTypes.string,
   }),
