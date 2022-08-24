f3285ded56c0e1384473c10a030c8bebe98071f4
andre09999 <andre09999@live.com>
2022-08-24 10:08:09 -0300

    req ate 18

--- src/Pages/Drinks.js
+++ src/Pages/Drinks.js
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function Drinks() {
   const history = useHistory();
   return (
     <div>
       {history.location.pathname === '/drinks' ? <Header title="Drinks" iconSearch />
+        : <p> Drinks </p>}
+      {history.location.pathname === '/drinks' ? <Footer />
         : <p> Drinks </p>}
     </div>
   );
--- src/Pages/Foods.js
+++ src/Pages/Foods.js
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function Foods() {
   const history = useHistory();
   return (
     <div>
       {history.location.pathname === '/foods' ? <Header title="Foods" iconSearch />
+        : <p> foods</p>}
+      {history.location.pathname === '/foods' ? <Footer />
         : <p> foods</p>}
     </div>
   );
