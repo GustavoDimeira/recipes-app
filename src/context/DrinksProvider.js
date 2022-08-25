d972b01ed6f7afa309f40870ef254c3e8176ba6b
Hugodesousa <hugodesousa.16@gmail.com>
2022-08-25 17:08:13 -0300

    anotaçoes no codigo

--- src/context/DrinksProvider.js
+++ src/context/DrinksProvider.js
@@ -6,6 +6,7 @@ import fetchDrinksApi from '../service/fetchDrinksApi';
 
 function DrinksProvider({ children }) {
   const history = useHistory();
+  // Estados iniciais;
   const [foods, setfoods] = useState({
     filter: {
       valueIngrents: '',
@@ -22,12 +23,11 @@ function DrinksProvider({ children }) {
     results,
     category,
   };
+  // userEffect e chamado ao alterar o estado "filtro", ele faz a requisição para API com os filtros aplicados, pela sideBar e salva o resultado no estado que renderiza os resultados;
   useEffect(() => {
     const fetchApi = async () => {
       const { valueIngrents, inputValue } = foods.filter;
       const request = await fetchDrinksApi(valueIngrents, inputValue, results);
-      console.log(request?.length === 0);
-      console.log(request);
       if (filtro && request?.length === 0) {
         global.alert('Sorry, we haven\'t found any recipes for these filters.');
       }
@@ -36,22 +36,23 @@ function DrinksProvider({ children }) {
     fetchApi();
     // }
   }, [filtro]);
-
+  // Requisição dos cards de foods iniciais;
   useEffect(() => {
     const fetchApi = async () => {
       const fet = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=').then((data) => data.json());
-   
+      // requisição para lista de categorias;
       setresults(fet);
       const cat = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list').then((data) => data.json());
       setcategory(cat);
     };
     fetchApi();
   }, []);
-
+  // Se vier apenas um resultado a pagina e redirecionada para a pagina de detalhes da receita;
   useEffect(() => {
     if (results?.drinks && results?.drinks.length === 1) {
       history.push(`/drinks/${results.drinks[0].idDrink}`);
     }
+    // Um alert e exido ao não retornar resultado;
     if (results?.length === 0) {
       global.alert('Sorry, we haven\'t found any recipes for these filters.');
     }
@@ -68,24 +69,3 @@ DrinksProvider.propTypes = {
 };
 
 export default DrinksProvider;
-
-// if (valueIngrents === 'Ingredient') {
-//   const fet = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${inputValue}`).then((data) => data.json());
-//   setresults(fet);
-// }
-// if (valueIngrents === 'Name') {
-//   const fet = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputValue}`).then((data) => data.json());
-//   setresults(fet);
-//   console.log(results);
-// }
-// if (valueIngrents === 'First letter') {
-//   if (inputValue.length === 1) {
-//     const fet = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${inputValue}`).then((data) => data.json());
-//     setresults(fet);
-//   } else {
-//     global.alert('Your search must have only 1 (one) character');
-//   }
-//   if (results?.length === 0) {
-//     global.alert('Sorry, we haven\'t found any recipes for these filters.');
-//   }
-// }
